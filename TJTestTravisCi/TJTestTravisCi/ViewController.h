0b95de0b2eb18cdb5b7a6b0c44eb931d27cc6344
malai <malai_520@163.com>
2017-10-10 15:24:28 +0800

    init

---
+++ TJTestTravisCi/TJTestTravisCi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TJTestTravisCi
+//
+//  Created by malai on 2017/10/10.
+//  Copyright © 2017年 malai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TJTestTravisCi/TJTestTravisCiTests/TJTestTravisCiTests.m
@@ -0,0 +1,43 @@
+//
+//  TJTestTravisCiTests.m
+//  TJTestTravisCiTests
+//
+//  Created by malai on 2017/10/10.
+//  Copyright © 2017年 malai. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "ViewController.h"
+
+@interface TJTestTravisCiTests : XCTestCase
+
+@end
+
+@implementation TJTestTravisCiTests
+
+- (void)setUp {
+    [super setUp];
+    
+    ViewController *vc = [ViewController new];
+    
+    XCTAssertNotNil(vc);
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
