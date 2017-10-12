ac039a614393a9f24c9fc338ade41c5a2655110d
malai <malai_520@163.com>
2017-10-12 11:11:32 +0800

    codedev

--- TJTestTravisCi/TJTestTravisCiTests/TJTestTravisCiTests.m
+++ TJTestTravisCi/TJTestTravisCiTests/TJTestTravisCiTests.m
@@ -34,7 +34,10 @@
 - (void)testPerformanceExample {
     // This is an example of a performance test case.
     [self measureBlock:^{
-        // Put the code you want to measure the time of here.
+        for (int i = 0; i < 1000000; i++) {
+            NSDate *date = [NSDate date];
+        }
+        
     }];
 }
 
